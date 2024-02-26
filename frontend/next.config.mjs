1fc21f543364583f4d1611d3567f1949f874f39b
Ratchanon <ratchanonmcnon2015@gmail.com>
2024-02-26 20:25:53 +0700

    Enable reactStrictMode in next.config.js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
-  output: `standalone`
+  reactStrictMode: true,
+  // output: `standalone`
 };
 
-// export default nextConfig;
-module.exports = nextConfig
+export default nextConfig;
+// module.exports = nextConfig
